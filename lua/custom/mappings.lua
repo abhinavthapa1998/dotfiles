3d9310dd374fa19a48225556f704ba4326a54e6d
Abhinav Thapa <abhinavthapa1998@gmail.com>
2024-01-02 19:24:05 +0530

    auto-center on scroll

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -8,6 +8,9 @@ M.general = {
     ["<C-l>"] = {"<cmd> TmuxNavigateRight<CR>", "window right"},
     ["<C-j>"] = {"<cmd> TmuxNavigateDown<CR>", "window down"},
     ["<C-k>"] = {"<cmd> TmuxNavigateUp<CR>", "window up"},
+    -- Center on scroll page
+    ["<C-d>"] = { "<C-d>zz", "Scroll down half page" },
+    ["<C-u>"] = { "<C-u>zz", "Scroll up half page" },
   },
   v = {
     [">"] = { ">gv", "indent" },
